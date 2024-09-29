7486fdf6c6b02e323d8693594648c993e3546e62
Shen Sat <shensat@shens-mbp.home>
2024-09-29 17:50:41 +0100

    Use array of edges

--- create_cursor.lua
+++ create_cursor.lua
@@ -2,29 +2,30 @@ function create_cursor(x,y)
   local color = 7
   local start_grid_x = 1
   local start_grid_y = 1
-  local start_edge = 'top'
   local cursor = {
     grid_x = start_grid_x,
     grid_y = start_grid_y,
-    edge = start_edge,
+    edge_index = 0,
+    edge = function(self)
+      local edges = {'top','right','bottom','left'}
+      local index = (self.edge_index % #edges) + 1
+      return edges[index]
+    end,
     draw = function(self)
       local cell = grid[self.grid_x][self.grid_y]
       local x = cell[1]
       local y = cell[2]
-      if self.edge == 'top' then
+      if self:edge() == 'top' then
         x += 6
-      elseif self.edge == 'right' then
+      elseif self:edge() == 'right' then
         x += 12
         y += 6
-      elseif self.edge == 'bottom' then
+      elseif self:edge() == 'bottom' then
         x += 6
         y += 13
-      elseif self.edge == 'left' then
+      elseif self:edge() == 'left' then
         y += 6
       end
-
-
-      
       pset(x,y,color)
     end,
     update = function(self)
@@ -34,33 +35,33 @@ function create_cursor(x,y)
         if grid[self.grid_x][next_grid_y] then self.grid_y -= 1 end
       -- right
       elseif btnp(1) then
-        if self.edge == 'top' then
+        if self:edge() == 'top' then
           local next_grid_y = self.grid_y + 1
           if grid[self.grid_x][next_grid_y] then 
             self.grid_y += 1
           else
-            self.edge = 'right'
+            self.edge_index += 1
           end
-        elseif self.edge == 'right' then
+        elseif self:edge() == 'right' then
           local next_grid_x = self.grid_x + 1
           if grid[self.grid_x][next_grid_x] then 
             self.grid_x += 1
           else
-            self.edge = 'bottom'
+            self.edge_index += 1
           end
-        elseif self.edge == 'bottom' then
+        elseif self:edge() == 'bottom' then
           local next_grid_y = self.grid_y - 1
           if grid[self.grid_x][next_grid_y] then 
             self.grid_y -= 1
           else
-            self.edge = 'left'
+            self.edge_index += 1
           end
-        elseif self.edge == 'left' then
+        elseif self:edge() == 'left' then
           local next_grid_x = self.grid_x - 1
           if grid[self.grid_x][next_grid_x] then 
             self.grid_x -= 1
           else
-            self.edge = 'top'
+            self.edge_index += 1
           end
         end
         
